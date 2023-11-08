e52ad002df995af1a1d8f09b894090ae1e322554
Spencer Stone <spencer.w.stone@gmail.com>
2023-11-07 18:28:37 -0800

    implement accepted mime types

--- src/config.h
+++ src/config.h
@@ -13,3 +13,27 @@ constexpr uint32_t ServerPort = 8080;
 
 // Server version
 std::string_view ServerVersion = read_file("../../VERSION").value_or("null");
+
+// Accepted MIME Types
+std::vector<std::pair<std::vector<std::string_view>, std::string_view>> ServerAcceptedMimeTypes = {
+        {{".txt", ".text", ".conf", ".log", ".ini"}, "text/plain"},
+        {{".html", ".htm"}, "text/html"},
+        {{".pdf"}, "application/pdf"},
+        {{".jpg", ".jpeg"}, "image/jpeg"},
+        {{".png"}, "image/png"},
+        {{".gif"}, "image/gif"},
+        {{".mp3"}, "audio/mpeg"},
+        {{".mp4"}, "video/mp4"},
+        {{".json"}, "application/json"},
+        {{".xml"}, "application/xml"},
+        {{".zip"}, "application/zip"},
+        {{".css"}, "text/css"},
+        {{".doc", ".docx"}, "application/msword"},
+        {{".xls", ".xlsx"}, "application/vnd.ms-excel"},
+        {{".ppt", ".pptx"}, "application/vnd.ms-powerpoint"},
+        {{".wav"}, "audio/wav"},
+        {{".avi"}, "video/x-msvideo"},
+        {{".bmp"}, "image/bmp"},
+        {{".csv"}, "text/csv"},
+        {{".js"}, "application/javascript"},
+        {{".ico"}, "image/x-icon"}};
--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,9 @@
 #include "cache.h"// initialize the cache at startup
 #include "server.h"
 #include "logger.h"
+#include "middleware.h"
 
-// Handle favicons
-auto favicon(const Request &req, Response &resp) -> void {
-    // is our request a route?
-    if (auto s = server.routes.find(req.path); s != server.routes.end()) {
-        return;
-    }
-
-    auto path = fs::path(req.path).filename();
-    if (path == "favicon.ico") {
-        if (auto content = cache[path]; content.has_value()) {
-            resp.set_type(ResponseType::Ok);
-            resp.set_header("Content-Type", "image/x-icon");
-            resp.set_content(content.value());
-        } else {
-            Logger::error("Unable to load favicon!");
-            resp = Response(ResponseType::InternalServerError);
-        }
-    }
-}
-
-// Handle requested path
-auto file_server(const Request &req, Response &resp) -> void {
-    // is our request a route?
-    if (auto s = server.routes.find(req.path); s != server.routes.end()) {
-        return;
-    }
-
-    auto path = fs::path(req.path).filename();
-    if (auto content = cache[path]; content.has_value()) {
-        resp.set_type(ResponseType::Ok);
-        resp.set_content(content.value());
-    }
-}
-
-// Handle file not found
-auto not_found(const Request &req, Response &resp) -> void {
-    // is our request a route?
-    if (auto s = server.routes.find(req.path); s != server.routes.end()) {
-        return;
-    }
-
-    auto path = fs::path(req.path).filename();
-    if (auto content = cache[path]; !content.has_value()) {
-        if (auto file = cache["404.html"]; file.has_value()) {
-            resp.set_type(ResponseType::NotFound);
-            resp.set_header("Content-Type", "text/html");
-            resp.set_content(file.value());
-            Logger::info(std::format("Client got 404: {}", req.path));
-        } else {
-            Logger::error("Unable to find 404 page!");
-            resp = Response(ResponseType::InternalServerError);
-        }
-    }
-}
-
-// Handle default index
+// Serve our index.html file when requesting /
 auto index_handler(const Request &req, Response &resp) -> void {
     if (req.path == fs::path("/")) {
         if (auto index = cache["index.html"]; index.has_value()) {
@@ -71,13 +17,13 @@ auto index_handler(const Request &req, Response &resp) -> void {
     }
 }
 
-// Handle test path
+// Test out a raw response
 auto test_handler(const Request &req, Response &resp) -> void {
     resp.set_type(ResponseType::Raw);
     resp.set_content("oh hey there");
 }
 
-// Handle echo path
+// Echo the clients request back
 auto echo_handler(const Request &req, Response &resp) -> void {
     resp.set_type(ResponseType::Ok);
     resp.set_header("Content-Type", "text/plain");
@@ -85,7 +31,7 @@ auto echo_handler(const Request &req, Response &resp) -> void {
 }
 
 auto main() -> int {
-    server.middleware(file_server, not_found, favicon);
+    server.middleware(file_server, not_found);
     server.route(
             Route{"/", index_handler},
             Route{"/test", test_handler},
