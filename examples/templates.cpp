c7542fc32cb623c372c92757415e1ee063a94b09
griefzz <spencer.w.stone@gmail.com>
2024-07-31 02:38:32 -0700

    fix bug in load_file not returning its result. Add async templatating methods.

--- examples/templates.cpp
+++ examples/templates.cpp
@@ -4,25 +4,59 @@
 /// |_||_|\__,_||_|  |_.__/\___/ \_,_||_|    License: MIT
 ///
 /// @file templates.cpp
-/// @brief Contains the example implementation of harbours html templating
+/// @brief Contains the example implementation of harbours templating
 
 #include <harbour/harbour.hpp>
 
 using namespace harbour;
 
+// Log a message to the console
 auto Hello() {
     log::info(tmpl::render("Hello from {}!", "Harbour"));
 }
 
-auto Ship() {
+// Load and render a template
+auto Render() {
     log::info("Rendering template...");
+    // First parameter is the file to render
+    // The other parameters are the arguments to use for rendering
     return tmpl::render_file("index.tmpl", "Harbour", "<h1>Ahoy!</h1>")
             .value_or("Couldn't compile template.");
 }
 
+// Load a file asynchronously and return it to the client
+auto AsyncLoad() -> awaitable<Response> {// Use an awaitable<Response> to get access to co_await/co_return
+    log::info("Async loading file...");
+
+    // Await the result of an async load_file
+    // First parameter is the file to load
+    // Second parameter is the completion token
+    auto file = co_await tmpl::load_file_async("index.html", use_awaitable);
+
+    // Return the result if file was found, otherwise return an error message
+    co_return file.value_or("Failed to load file");
+}
+
+// Render a file asynchronously and return it to the client
+auto AsyncRender() -> awaitable<Response> {
+    log::info("Async rendering template...");
+
+    // Await the result of an async render_file
+    // First parameter is the completion handler,
+    // Second parameter is file to render
+    // Third parameter is the arguments to pass in for rendering
+    auto file = co_await tmpl::render_file_async(use_awaitable, "index.tmpl", "Hello!");
+
+    // Return the result if file was found, otherwise return an error message
+    co_return file.value_or("Couldn't compile template.");
+}
+
 auto main() -> int {
     Harbour hb;
-    hb.dock(Hello, Ship);
+    hb.dock("/hello", Hello)
+      .dock("/render", Render)
+      .dock("/async/load", AsyncLoad)
+      .dock("/async/render", AsyncRender);
     hb.sail();
     return 0;
 }
--- include/harbour/template.hpp
+++ include/harbour/template.hpp
@@ -13,6 +13,8 @@
 #include <iterator>
 #include <filesystem>
 
+#include <asio.hpp>
+
 #include <fmt/core.h>
 #include <fmt/format.h>
 
@@ -30,9 +32,56 @@ namespace harbour {
 
             std::ifstream file(std::string(path), std::ios::binary);
             if (file.is_open())
-                std::string((It(file)), It());
+                return std::string((It(file)), It());
 
             return {};
+        }
+
+        namespace detail {
+
+            /// @brief load file with callback used in load_file_async
+            template<typename Callback>
+            void load_file_impl(const std::string_view path, Callback cb) {
+                std::thread(
+                        [path, cb = std::move(cb)]() mutable {
+                            auto data = load_file(path);
+                            std::move(cb)(std::move(data));
+                        })
+                        .detach();
+            }
+
+        }// namespace detail
+
+        /// @brief Loads the content of a file into a string using a completion token
+        /// @tparam CompletionToken Completion Token for async operation
+        /// @param path Path to the file.
+        /// @param token Token to specify async operation type (asio::use_future, asio::use_awaitable)
+        /// @return Optional string containing the file content.
+        template<asio::completion_token_for<void(std::optional<std::string>)> CompletionToken>
+        auto load_file_async(const std::string_view path, CompletionToken &&token) {
+            auto init = [](asio::completion_handler_for<void(std::optional<std::string>)> auto handler,
+                           const std::string_view path) {
+                auto work = asio::make_work_guard(handler);
+
+                detail::load_file_impl(path,
+                                       [handler = std::move(handler),
+                                        work    = std::move(work)](std::optional<std::string> result) mutable {
+                                           auto alloc = asio::get_associated_allocator(
+                                                   handler, asio::recycling_allocator<void>());
+
+                                           asio::dispatch(work.get_executor(),
+                                                          asio::bind_allocator(alloc,
+                                                                               [handler = std::move(handler),
+                                                                                result  = std::optional<std::string>(result)]() mutable {
+                                                                                   std::move(handler)(result);
+                                                                               }));
+                                       });
+            };
+
+            return asio::async_initiate<CompletionToken, void(std::optional<std::string>)>(
+                    init,
+                    token,
+                    path);
         }
 
         /// @brief Renders a template string with the provided arguments.
@@ -52,6 +101,45 @@ namespace harbour {
                 return fmt::vformat(*data, fmt::make_format_args(args...));
 
             return {};
+        }
+
+        namespace detail {
+            /// @brief render a file with callback used in render_file_async
+            template<typename Callback>
+            void render_file_impl(Callback cb, const std::string_view path, const auto &...args) {
+                std::thread(
+                        [path, cb = std::move(cb), args...]() mutable {
+                            auto data = render_file(path, args...);
+                            std::move(cb)(std::move(data));
+                        })
+                        .detach();
+            }
+
+        }// namespace detail
+
+        template<asio::completion_token_for<void(std::optional<std::string>)> CompletionToken>
+        auto render_file_async(CompletionToken &&token, const std::string_view path, const auto &...args) {
+            auto init = [](asio::completion_handler_for<void(std::optional<std::string>)> auto handler,
+                           const std::string_view path, const auto &...args) {
+                auto work = asio::make_work_guard(handler);
+
+                detail::render_file_impl([handler = std::move(handler), work = std::move(work)](std::optional<std::string> result) mutable {
+                                           auto alloc = asio::get_associated_allocator(
+                                                   handler, asio::recycling_allocator<void>());
+
+                                           asio::dispatch(work.get_executor(),
+                                                          asio::bind_allocator(alloc,
+                                                                               [handler = std::move(handler),
+                                                                                result  = std::optional<std::string>(result)]() mutable {
+                                                                                   std::move(handler)(result);
+                                                                               })); }, path, (args)...);
+            };
+
+            return asio::async_initiate<CompletionToken, void(std::optional<std::string>)>(
+                    init,
+                    token,
+                    path,
+                    (args)...);
         }
 
     }// namespace tmpl
